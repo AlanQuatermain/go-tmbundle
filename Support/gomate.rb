63d0e32684ca9ef6c78003e02fb354a42115e9d2
Martin Kühl <martin.kuehl@gmail.com>
2010-06-11 00:20:31 +0800

    Hyperlink errors in command output.
    The Go toolchain currently reports errors on stdout, not stderr, which
    would help distinguish errors from legitimate output. We currently work
    around this by redirecting most output from the toolchain to stderr
    manually, but can't do this for tools like `gomake` which mix both kinds
    of output and invoke their subcommands themselves. As a result, errors
    in `gomake` output won't be hyperlinked.

--- Support/gomate.rb
+++ Support/gomate.rb
@@ -1,9 +1,8 @@
 #!/usr/bin/env ruby
 
-SUPPORT = ENV['TM_SUPPORT_PATH']
-require "#{SUPPORT}/lib/escape"
-require "#{SUPPORT}/lib/tm/executor"
-require "#{SUPPORT}/lib/tm/save_current_document"
+require "#{ENV['TM_SUPPORT_PATH']}/lib/tm/executor"
+require "#{ENV['TM_SUPPORT_PATH']}/lib/tm/save_current_document"
+require "#{ENV['TM_BUNDLE_SUPPORT']}/goerrs"
 
 TextMate.save_current_document('go')
 TextMate::Executor.make_project_master_current_document
@@ -13,5 +12,7 @@ def gomate(tool, verb = nil)
   file = ENV['TM_FILEPATH']
   opts = {:interactive_input => false}
   opts[:verb] = verb  if verb
-  TextMate::Executor.run(tool, file, opts)
+  TextMate::Executor.run(tool, file, opts) do |str, type|
+    Go::link_errs(str, type)
+  end
 end
